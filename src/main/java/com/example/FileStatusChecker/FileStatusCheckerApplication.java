d1b847751a0ba1acffcc0e7a61b4dafba10ed507
Jonathan Potter <jonpot@umich.edu>
2020-08-19 17:09:44 -0400

    Add code from spring intializer

---
+++ src/main/java/com/example/FileStatusChecker/FileStatusCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FileStatusChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStatusCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStatusCheckerApplication.class, args);
+	}
+
+}
