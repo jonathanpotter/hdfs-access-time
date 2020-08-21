5a082e5bb3be47a9560d5e3a894f074e21d2863f
Jonathan Potter <jonpot@umich.edu>
2020-08-21 14:57:47 -0400

    Add recursive walk

--- src/main/java/com/example/FileStatusChecker/FileStatusCheckerApplication.java
+++ src/main/java/com/example/FileStatusChecker/FileStatusCheckerApplication.java
@@ -3,7 +3,7 @@ package com.example.FileStatusChecker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import java.io.*;
+import java.io.*;
 //import java.util.*;
 //import java.net.*;
 import org.apache.hadoop.fs.*;
@@ -23,9 +23,45 @@ import java.util.Date;
 @SpringBootApplication
 public class FileStatusCheckerApplication {
 
+        public void walk( FileSystem fs, String hdfsFilePath ) {
+        //public static String walk( String path ) {
+
+            try {
+	        FileStatus[] status = fs.listStatus(new Path(hdfsFilePath));  // you need to pass in your hdfs path
+                //System.out.printf("%-82s%-22s\n","File","Last Accessed");
+	        for (FileStatus fileStatus : status) {
+                    if ( fileStatus.isDirectory() ) {
+                        walk(fs, fileStatus.getPath().toString());
+
+                    } else {
+	                long lastAccessTimeLong = fileStatus.getAccessTime();
+	                Date lastAccessTimeDate = new Date(lastAccessTimeLong);
+	                DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+                        System.out.printf("%-82s%-22s\n",fileStatus.getPath().toString(),df.format(lastAccessTimeDate));
+                    }
+                }
+            } catch(Exception e) {
+               	System.out.println("File not found");
+               	e.printStackTrace();
+            }
+            //File root = new File( path );
+            //File[] list = root.listFiles();
+
+            //if (list == null) return;
+
+            //for ( File f : list ) {
+            //    if ( f.isDirectory() ) {
+            //        walk( f.getAbsolutePath() );
+            //        System.out.println( "Dir:" + f.getAbsoluteFile() );
+            //    }
+            //    else {
+            //        System.out.println( "File:" + f.getAbsoluteFile() );
+            //    }
+            //}
+        }
+
 	public static void main(String[] args) {
 		SpringApplication.run(FileStatusCheckerApplication.class, args);
-		System.out.println("Getting last accessed time.");
 
 		try {
                         // Create config for Cavium ThunderX.
@@ -34,20 +70,15 @@ public class FileStatusCheckerApplication {
                         conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
                         conf.set("fs.default.name", "hdfs://cavium-nn02");
 
-                        UserGroupInformation.setConfiguration(conf);
                         // Subject is taken from current user context
+                        UserGroupInformation.setConfiguration(conf);
                         UserGroupInformation.loginUserFromSubject(null);
 
 			FileSystem fs = FileSystem.get(conf);
-			String hdfsFilePath = "hdfs://cavium-nn02/user/jonpot/aa.txt";
-			FileStatus[] status = fs.listStatus(new Path(hdfsFilePath));  // you need to pass in your hdfs path
+			String hdfsFilePath = "/user/jonpot/";
 
-			for (FileStatus fileStatus : status) {
-				long lastAccessTimeLong = fileStatus.getAccessTime();
-				Date lastAccessTimeDate = new Date(lastAccessTimeLong);
-				DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
-				System.out.println("The file '" + hdfsFilePath + "' was accessed last at: " + df.format(lastAccessTimeDate));
-			}
+                        FileStatusCheckerApplication fw = new FileStatusCheckerApplication();
+                        fw.walk(fs, hdfsFilePath);
 
 		} catch(Exception e) {
 			System.out.println("File not found");
