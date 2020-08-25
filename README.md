4fa25c7df133f46d65eb8e69310e1779448bafca
Jonathan Potter <jonpot@umich.edu>
2020-08-25 13:50:12 -0400

    Add script for checking all of /user

--- README.md
+++ README.md
@@ -1,23 +1,37 @@
-Checks the last access time of a file in HDFS.
+Checks the last access time of files in HDFS.
 
 Code from https://community.cloudera.com/t5/Support-Questions/Is-there-anyway-to-get-last-access-time-of-hdfs-files/td-p/153375
 
-# Use It
+Given N number of days and a path, the program will recursively check all files in the path and list out those that have not been accessed in the last N days. The output is a list of files separated by the null character (for easy processing by du). The full path to the file is printed prefixed by `/hadoop-fuse`.
+
+# Usage
 
 ```bash
+java -jar FileStatusChecker*.jar PATH -atime DAYS
+
 # Clone repo to hadoop login node and build jar.
 ./gradlew clean build
 
 # Run it against your own directory.
-java -jar build/libs/FileStatusChecker-0.0.1-SNAPSHOT.jar 90 /user/jonpot
+java -jar build/libs/FileStatusChecker-0.0.1-SNAPSHOT.jar /user/jonpot -atime 90 | tr '\0' '\n'
 
 # Or send output to du to calculate usage.
-java -jar build/libs/FileStatusChecker-0.0.1-SNAPSHOT.jar 90 /user/jonpot | sed -e 's/^/\/hadoop-fuse/' | xargs du -h -c
+java -jar build/libs/FileStatusChecker-0.0.1-SNAPSHOT.jar /user/jonpot -atime 90 | du -s -h -c --files0-from=- | grep total | tail -n1
 
 # Or run it as user hdfs on hadoop login node to calculate all user directories.
 cp ~/workspace/hdfs-access-time/build/libs/FileStatusChecker-0.0.1-SNAPSHOT.jar /tmp/
 # switch to root.
-sudo -u hdfs bash -c 'java -jar /tmp/FileStatusChecker-0.0.1-SNAPSHOT.jar 90 /user/jonpot' | sed -e 's/^/\/hadoop-fuse/' | xargs du -h -c
+USER_HOME_DIR=/hadoop-fuse/user/*
+OUTPUT_FILE=last-access-audit-$(date "+%Y%m%d").txt
+> ${OUTPUT_FILE}
+for dir in ${USER_HOME_DIR}; do
+    user="$(basename ${dir})"
+    echo -n "${user}," >> ${OUTPUT_FILE}
+    sudo user=${user} -u hdfs bash -c 'java -jar /tmp/FileStatusChecker-0.0.1-SNAPSHOT.jar /user/${user} -atime 180' | du -s -h -c --files0-from=- | tail -n 1 | cut -f 1 >> ${OUTPUT_FILE}
+done
+
+# Sort the output by size.
+awk 'BEGIN { FS = "," } ; { print $2,$1 }' ${OUTPUT_FILE} | sort -hr
 
 #echo `hadoop classpath`
 #javac -cp `hadoop classpath` FileStatusChecker.java
--- src/main/java/com/example/FileStatusChecker/FileStatusCheckerApplication.java
+++ src/main/java/com/example/FileStatusChecker/FileStatusCheckerApplication.java
@@ -38,7 +38,10 @@ public class FileStatusCheckerApplication {
 	                Date lastAccessTimeDate = new Date(lastAccessTimeLong);
 	                DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
                         if ( limitTimestamp > lastAccessTimeLong ) {
-                            System.out.printf("%-82s\n",fileStatus.getPath().getPathWithoutSchemeAndAuthority(fileStatus.getPath()).toString());
+                            //System.out.printf("%-82s\n",fileStatus.getPath().getPathWithoutSchemeAndAuthority(fileStatus.getPath()).toString());
+                            System.out.printf("/hadoop-fuse%s\u0000",fileStatus.getPath().getPathWithoutSchemeAndAuthority(fileStatus.getPath()).toString());
+                            //System.out.printf("\u0000");
+                            //char c='\u0000';
                             //System.out.println("└── " + df.format(lastAccessTimeDate));
                         }
                     }
@@ -53,19 +56,64 @@ public class FileStatusCheckerApplication {
 		SpringApplication.run(FileStatusCheckerApplication.class, args);
 
 		try {
+                        // Set defaults
+                        long limitInDays = 90;
+
+                        // Evaluate arguments
+                        for (int i = 0; i < args.length; i++) {
+                            if (args[i].equals("-atime")) {
+                                if (i+1 < args.length) {
+                                    limitInDays = Long.parseLong(args[i+1]);
+                                }
+                                else {
+                                    System.out.println("Error: Option -atime requires a value.");
+                                    System.exit(1);
+                                }
+                            }
+
+                            //switch (month) {
+                            //    case 1:  monthString = "January";
+                            //             break;
+                            //    case 2:  monthString = "February";
+                            //             break;
+                            //    case 3:  monthString = "March";
+                            //             break;
+                            //    case 4:  monthString = "April";
+                            //             break;
+                            //    case 5:  monthString = "May";
+                            //             break;
+                            //    case 6:  monthString = "June";
+                            //             break;
+                            //    case 7:  monthString = "July";
+                            //             break;
+                            //    case 8:  monthString = "August";
+                            //             break;
+                            //    case 9:  monthString = "September";
+                            //             break;
+                            //    case 10: monthString = "October";
+                            //             break;
+                            //    case 11: monthString = "November";
+                            //             break;
+                            //    case 12: monthString = "December";
+                            //             break;
+                            //    default: monthString = "Invalid month";
+                            //             break;
+                            //}
+
+                        }
+
                         // Define which recent files will be ignored in days from current
-                        long limitInDays = Long.parseLong(args[0]);
                         long limitInMillis = limitInDays * 24 * 60 * 60 * 1000;
 
                         long currentTimestamp = System.currentTimeMillis();
                         long limitTimestamp = currentTimestamp - limitInMillis;
                         
-                        System.out.println("Listing files that have not been accessed in last " + limitInDays + " days.");
                         DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
                         Date currentTimeDate = new Date(currentTimestamp);
                         Date limitTimeDate = new Date(limitTimestamp);
-                        System.out.println("Current Date: " + df.format(currentTimeDate));
-                        System.out.println("Limit Date: " + df.format(limitTimeDate)); 
+                        //System.out.println("Listing files that have not been accessed in last " + limitInDays + " days.");
+                        //System.out.println("Current Date: " + df.format(currentTimeDate));
+                        //System.out.println("Limit Date: " + df.format(limitTimeDate));
 
                         // Create config for Cavium ThunderX.
                         Configuration conf = new Configuration();
@@ -78,7 +126,7 @@ public class FileStatusCheckerApplication {
                         UserGroupInformation.loginUserFromSubject(null);
 
 			FileSystem fs = FileSystem.get(conf);
-			String hdfsFilePath = args[1];
+			String hdfsFilePath = args[0];
                         FileStatusCheckerApplication fw = new FileStatusCheckerApplication();
                         fw.walk(fs, hdfsFilePath, limitTimestamp);
 
