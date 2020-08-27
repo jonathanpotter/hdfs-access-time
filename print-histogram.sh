02183d4a47933ef4a113a304539cadafea564034
Jonathan Potter <jonpot@umich.edu>
2020-08-27 16:44:35 -0400

    Add file size to histogram

--- print-histogram.sh
+++ print-histogram.sh
@@ -7,19 +7,20 @@ read_input() {
     millisPerDay=$((1000 * 60 * 60 * 24))
     minAccessTime=${currentTime}
     maxAccessTime=1
-    maxDaysAllowed=1460
-    fileCount=0
-    groupSize=30
-    groupCount=$((${maxDaysAllowed} / ${groupSize}))
-    # Initialize histogram array.
+    maxDaysAllowed=1460             # Upper limit of oldest access time in days.
+    fileCountSum=0                  # Running total count of all files.
+    groupSize=30                    # Number of days in each histogram group. 
+    groupCount=$((${maxDaysAllowed} / ${groupSize}))    # Number of histogram groups.
     for ((i=0;i<${groupCount};i++)); do
-        groupValue[$i]=0
+        groupFileCount[$i]=0        # Running total count of files in group.
+        groupFileSize[$i]=0         # Running total sum of file sizes in group.
     done
 
     # Each line is a file. First field is file path. Second field is atime. 
     while read line
     do
         accessTime=$(echo "$line" | cut -d "," -f 2)
+        fileSize=$(echo "$line" | cut -d "," -f 3)
 
         # If atime is larger or smaller than any previous, set new min or max.
         if [ "$accessTime" -lt "$minAccessTime" ]; then
@@ -37,10 +38,10 @@ read_input() {
             exit 1
         fi
         groupId=$((${ageDays} / ${groupSize}))
-        ((groupValue[${groupId}]+=1))
-        ((fileCount+=1))
+        ((groupFileCount[${groupId}]+=1))
+        ((groupFileSize[${groupId}]+=${fileSize}))
+        ((fileCountSum+=1))
     done < "${1:-/dev/stdin}"
-
 }
 
 print_results() {
@@ -53,18 +54,20 @@ print_results() {
     echo "   =============="
     echo "   Atime results:"
     echo "   =============="
-    echo "   Total files:  ${fileCount}"
-    if [ ${fileCount} -gt 0 ]; then
+    echo "   Total files:  ${fileCountSum}"
+    if [ ${fileCountSum} -gt 0 ]; then
         #echo "   Average atime age in days:  127.981  days"
         echo "   Oldest atime age file in days:  ${oldestAccessTimeDay}  days"
         echo "   Youngest atime age file in days:  ${youngestAccessTimeDay}  days"
         #echo "   Standard deviation atime age in days:  98.5828  days"
         echo ""
+        echo "      LAST ACCESSED         FILE COUNT        SIZE (GB)"
         lastGroup=$(((${oldestAccessTimeDay} / ${groupSize}) + 1))
         for ((i=0;i<${lastGroup};i++)); do
             groupStartAge=$(($i * ${groupSize}))
             groupEndAge=$((($i * ${groupSize}) + ${groupSize}))
-            printf "  [ %4s  - %4s days ]:    %6s\n" ${groupStartAge} ${groupEndAge} ${groupValue[$i]}
+            groupFileSizeGB=$(echo "scale=1; ${groupFileSize[$i]} / 1024 / 1024 / 1024" | bc)
+            printf "  [ %4s  - %4s days ]         %6s         %8s\n" ${groupStartAge} ${groupEndAge} ${groupFileCount[$i]} ${groupFileSizeGB}
         done
     fi
     echo ""
