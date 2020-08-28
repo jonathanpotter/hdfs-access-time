8f6c4ae765e3570a7e4ecaa7943591ef0c623e17
Jonathan Potter <jonpot@umich.edu>
2020-08-28 14:52:06 -0400

    Add % and rename group to bin

--- print-histogram.sh
+++ print-histogram.sh
@@ -7,17 +7,15 @@ read_input() {
     millisPerDay=$((1000 * 60 * 60 * 24))
     minAccessTime=${currentTime}
     maxAccessTime=1
-    #maxDaysAllowed=1460             # Upper limit of oldest access time in days.
     maxDaysAllowed=1825             # Upper limit of oldest access time in days.
     fileCountSum=0                  # Running total count of all files.
     fileSizeSum=0                   # Running total sum of all file sizes.
-    groupSize=30                    # Number of days in each histogram group. 
-    groupCount=$((${maxDaysAllowed} / ${groupSize}))    # Number of histogram groups.
-    for ((i=0;i<${groupCount};i++)); do
-        groupFileCount[$i]=0        # Running total count of files in group.
-        groupFileSize[$i]=0         # Running total sum of file sizes in group.
+    binSize=30                      # Number of days in each histogram bin. 
+    binCount=$((${maxDaysAllowed} / ${binSize}))    # Number of histogram bins.
+    for ((i=0;i<${binCount};i++)); do
+        binFileCount[$i]=0          # Running total count of files in bin.
+        binFileSize[$i]=0           # Running total sum of file sizes in bin.
     done
-
 
     # Each line is a file. First field is file path. Second field is atime. 
     while IFS="," read -r ignorePath accessTime fileSize
@@ -31,16 +29,16 @@ read_input() {
             maxAccessTime=${accessTime}
         fi
 
-        # Increment the corresponding histogram group.
+        # Increment the corresponding histogram bin.
         ageDays=$(((${currentTime} - ${accessTime}) / ${millisPerDay}))
         if [ "$ageDays" -gt "$maxDaysAllowed" ]; then
             echo "Some files have not been accessed in more than ${maxDaysAllowed} days."
             echo "The utility does not support files this old."
             exit 1
         fi
-        groupId=$((${ageDays} / ${groupSize}))
-        ((groupFileCount[${groupId}]+=1))
-        ((groupFileSize[${groupId}]+=${fileSize}))
+        binId=$((${ageDays} / ${binSize}))
+        ((binFileCount[${binId}]+=1))
+        ((binFileSize[${binId}]+=${fileSize}))
         ((fileCountSum+=1))
         ((fileSizeSum+=${fileSize}))
     done < "${1:-/dev/stdin}"
@@ -60,16 +58,17 @@ print_results() {
         echo "   Youngest atime age file in days:  ${youngestAccessTimeDay}  days"
         #echo "   Standard deviation atime age in days:  98.5828  days"
         echo ""
-        echo "      LAST ACCESSED         FILE COUNT        SIZE (GB)"
-        lastGroup=$(((${oldestAccessTimeDay} / ${groupSize}) + 1))
-        for ((i=0;i<${lastGroup};i++)); do
-            groupStartAge=$(($i * ${groupSize}))
-            groupEndAge=$((($i * ${groupSize}) + ${groupSize}))
-            groupFileSizeGB=$(echo "scale=1; ${groupFileSize[$i]} / 1024 / 1024 / 1024" | bc)
-            printf "  [ %4s  - %4s days ]         %6s         %8s\n" ${groupStartAge} ${groupEndAge} ${groupFileCount[$i]} ${groupFileSizeGB}
-        done
+        echo "      LAST ACCESSED         FILE COUNT     SIZE (GB)     SIZE (%)"
         fileSizeSumGB=$(echo "scale=1; ${fileSizeSum} / 1024 / 1024 / 1024" | bc)
-        printf "      TOTAL       %20s         %8s\n" ${fileCountSum} ${fileSizeSumGB}
+        lastbin=$(((${oldestAccessTimeDay} / ${binSize}) + 1))
+        for ((i=0;i<${lastbin};i++)); do
+            binStartAge=$(($i * ${binSize}))
+            binEndAge=$((($i * ${binSize}) + ${binSize}))
+            binFileSizeGB=$(echo "scale=1; ${binFileSize[$i]} / 1024 / 1024 / 1024" | bc)
+            binFileSizePercent=$(echo "scale=3; 100 * ${binFileSize[$i]} / ${fileSizeSum}" | bc)
+            printf "  [ %4s  - %4s days ]       %8s      %8s        %5.1f\n" ${binStartAge} ${binEndAge} ${binFileCount[$i]} ${binFileSizeGB} ${binFileSizePercent}
+        done
+        printf "      TOTAL       %20s      %8s        100.0\n" ${fileCountSum} ${fileSizeSumGB}
     fi
     echo ""
     # See https://www.admin-magazine.com/HPC/Articles/Understanding-the-Status-of-Your-Filesystem
